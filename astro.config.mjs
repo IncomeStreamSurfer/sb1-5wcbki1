4f8f253ab88801a016ece1de20f614ae3dc5d14f
IncomeStreamSurfer <git@stackblitz.com>
2024-10-12 11:22:33 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from 'astro-sitemap';
+
+export default defineConfig({
+  site: 'https://theclassicmenswearchronicle.com',
+  integrations: [
+    tailwind(),
+    sitemap()
+  ]
+});
